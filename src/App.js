8dedecc22397fcb5a6c27003774792f4526b2fed
scrabill <shannon.crabill@gmail.com>
2020-04-03 11:42:21 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; /* code change - Allows a component to access the changed states in the store */
 import './App.css';
 
 class App extends Component {
@@ -21,4 +22,13 @@ class App extends Component {
   }
 };
 
-export default App;
+// start of code change
+// Synces with the store, looking for a change in state that we specify below
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+
+// Gives the component access to the changed state (now props) that we specified above
+// The component that we want to update is specified and exported
+export default connect(mapStateToProps)(App);
+// end of code change
